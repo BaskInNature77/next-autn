66dac423e9820eb1792b66fa47127d88e0363d2e
BaskInNature77 <82895982+BaskInNature77@users.noreply.github.com>
2021-05-19 23:51:24 +0530

    Update protected.js

--- pages/api/examples/protected.js
+++ pages/api/examples/protected.js
@@ -7,6 +7,6 @@ export default async (req, res) => {
   if (session) {
     res.send({ content: 'This is protected content. You can access this content because you are signed in.' })
   } else {
-    res.send({ error: 'You must be sign in to view the protected content on this page.' })
+    res.send({ error: 'Dear user, You must be sign in to view the protected content on this page.' })
   }
 }
