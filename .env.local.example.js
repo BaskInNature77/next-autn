df82433e1a17f179de58717fe8cfd6d8b4bc91c9
BaskInNature77 <82895982+BaskInNature77@users.noreply.github.com>
2021-05-19 22:47:49 +0530

    init

--- .env.local.example.js
+++ .env.local.example.js
@@ -1,5 +1,5 @@
 NEXTAUTH_URL=http://localhost:3000
-SECRET= # Linux: `openssl rand -hex 32` or go to https://generate-secret.now.sh/32
+SECRET=f244e047f67de67a901d78949c8d35d5
 
 APPLE_ID=
 APPLE_TEAM_ID=
@@ -16,8 +16,8 @@ FACEBOOK_SECRET=
 GITHUB_ID=
 GITHUB_SECRET=
 
-GOOGLE_ID=
-GOOGLE_SECRET=
+GOOGLE_ID=72436567158-f1dhpn5ackce32svgt15oql092e011kq.apps.googleusercontent.com
+GOOGLE_SECRET=R9EDdXDjwCEsMLi77G72K1w1
 
 TWITTER_ID=
 TWITTER_SECRET=
@@ -25,4 +25,4 @@ TWITTER_SECRET=
 EMAIL_SERVER=smtp://username:password@smtp.example.com.com:587
 EMAIL_FROM=NextAuth <noreply@example.com>
 
-DATABASE_URL=sqlite://localhost/:memory:?synchronize=true
+DATABASE_URL=mongodb+srv://baskinnature:3V6lxDhOpU7ujLnL@cluster0.x3arp.mongodb.net/Store?retryWrites=true&w=majority
